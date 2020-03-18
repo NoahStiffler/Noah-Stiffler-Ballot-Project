37c0c3bf658900a299fd57df7e9697713b27643e
Noah Stiffler <stifflernoah@gmail.com>
2020-03-18 13:14:52 -0400

    Initial Commit

---
+++ Noah Stiffler Ballot Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Noah Stiffler Ballot Project
+//
+//  Created by Noah Stiffler on 3/18/20.
+//  Copyright © 2020 Noah Stiffler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
